7fc5463971a860898dab2be1e2db1166b8690f04
bhagat-abhishek <inst.abhishek@gmail.com>
2022-04-05 02:26:49 +0530

    Added Fields for Migration Department

--- database/migrations/2022_04_04_205306_create_departments_table.php
+++ database/migrations/2022_04_04_205306_create_departments_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
+            $table->string('dept_id');
+            $table->string('dept_name');
             $table->timestamps();
         });
     }
