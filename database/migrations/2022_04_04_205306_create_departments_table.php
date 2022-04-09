f19819b6d8df2c55b895524f9eb958286a56b85c
bhagat-abhishek <inst.abhishek@gmail.com>
2022-04-09 22:31:43 +0530

    Database tables Modifications

--- database/migrations/2022_04_04_205306_create_departments_table.php
+++ database/migrations/2022_04_04_205306_create_departments_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('deptid');
             $table->string('name');
-            $table->string('status');
+            $table->string('status')->default('active');
             $table->timestamps();
         });
     }
